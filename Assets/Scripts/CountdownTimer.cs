dd5f37a194ba2e599a6a1b78371021092d192e8c
CryoTechnic <cryogen65@gmail.com>
2022-03-05 18:55:40 -0500

    added timer scripts

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+   public int countdownTime;
+   public Text countdownText;
+
+   private void Start() {
+       StartCoroutine(countdownTimeLeft());   
+   }
+   IEnumerator countdownTimeLeft(){
+         while(countdownTime > 0){
+             countdownText.text = countdownTime.ToString();
+             yield return new WaitForSeconds(1f);
+             countdownTime--;
+         }
+   countdownText.text = "Time's Up!";
+   yield return new WaitForSeconds(1f);
+   countdownText.text = "Restarting level...";
+   SceneManager.LoadScene("Level1");
+   }
+
+}
