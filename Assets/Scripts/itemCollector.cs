d4f517799035f1e78bda1b8c486dc94e967e9733
CryoTechnic <cryogen65@gmail.com>
2022-03-05 19:42:11 -0500

    added gem count tracker on a scene-by-scene basis

--- Assets/Scripts/itemCollector.cs
+++ Assets/Scripts/itemCollector.cs
@@ -6,14 +6,16 @@ using UnityEngine.SceneManagement;
 public class itemCollector : MonoBehaviour
 {
     public AudioClip collectSound;
-    public static int gemCount;
+    public static int gemCountLevel1;
+    public static int gemCountLevel2;
 
     private void Start() {
-        gemCount = 1;   
+        gemCountLevel1 = 8;
+        gemCountLevel2 = 8;
     }
 
     private void Update() {
-        if (gemCount == 0) {
+        if (gemCountLevel1 == 0) {
             SceneManager.LoadScene("Level2");
         }
     }
@@ -25,7 +27,17 @@ public class itemCollector : MonoBehaviour
             AudioSource.PlayClipAtPoint(collectSound, transform.position);
             ScoreSystem.score += 50;
             Destroy(gameObject);
-            gemCount--;
+            Scene currentScene = SceneManager.GetActiveScene();
+            string sceneName = currentScene.name;   
+            if ((SceneManager.GetActiveScene().name).ToString() == "Level1") {
+                gemCountLevel1--;
+            }
+            else if ((SceneManager.GetActiveScene().name).ToString() == "Level2") {
+                gemCountLevel2--;
+            }
+            else {
+                Debug.Log("Error: Scene not found");
+            }
         }
     }
 }
