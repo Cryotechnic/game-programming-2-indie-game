0792461fc77d0dc338b45b769942c7d21af8c0e8
CryoTechnic <cryogen65@gmail.com>
2022-04-18 19:02:19 -0400

    fixed cursor being displayed from main menu after loading next level

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,9 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Hide cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void QuitGame() {
