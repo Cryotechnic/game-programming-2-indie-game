17a40d1bccebe2b6a7af5867af854bf69b561d74
CryoTechnic <cryogen65@gmail.com>
2022-04-18 19:02:59 -0400

    Fixed score not updating and being reset to 0 every time a level is loaded

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -2,18 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreSystem : MonoBehaviour
 {
    public GameObject scoreText;
-   public static int score;
+   public GameObject levelName;
+   public static int scoreLv1;
+   public static int scoreLv2 = 0;
+   int combinedScore = 0;
 
     private void Start() {
-        score = 0; // FIXME: Will this be reset every time the scene is loaded
+        // scoreLv1 = 0; // FIXME: Uncommenting this line WILL break the score system.
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if ((SceneManager.GetActiveScene().name).ToString() == "Level1") {
+            levelName.GetComponent<Text>().text = "Level 1";
+        }
+        else if ((SceneManager.GetActiveScene().name).ToString() == "Level2") {
+            levelName.GetComponent<Text>().text = "Level 2";
+        }
+        else {
+            Debug.Log("Error: Scene not found");
+        }
     }
     // Update is called once per frame
     void Update()
     {
-        scoreText.GetComponent<Text>().text = "Score: " + score;
+        // Checks which level is loaded
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if ((SceneManager.GetActiveScene().name).ToString() == "Level1") {
+            scoreText.GetComponent<Text>().text = "Score: " + scoreLv1;
+        }
+        else if ((SceneManager.GetActiveScene().name).ToString() == "Level2") {
+            combinedScore = scoreLv1 + scoreLv2;
+            scoreText.GetComponent<Text>().text = "Score: " + combinedScore;
+        }
+        else {
+            Debug.Log("Error: Scene not found");
+        }
+        // scoreText.GetComponent<Text>().text = "Score: " + scoreLv1;
     }
 }
